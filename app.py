0109f7a02577feda529f6f949aaea79852e4068d
KrithikShaileshR <krithik.shailesh@agraga.com>
2025-03-04 22:36:35 +0530

    Added shared secret logic

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 from resume_parser import extract_text_from_pdf
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+SHARED_SECRET = os.getenv('SHARED_SECRET')
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+def require_secret_key(view_function):
+    def wrapper(*args, **kwargs):
+        # Check if the request contains the correct secret key in the headers
+        if request.headers.get('X-Secret-Key') != SHARED_SECRET:
+            abort(401, description="Unauthorized: Invalid secret key")
+        return view_function(*args, **kwargs)
+    return wrapper
+
 @app.route("/parse", methods=["POST"])
+@require_secret_key
 def parse_resume_endpoint():
     if "file" not in request.files:
         return jsonify({"error": "No file uploaded"}), 400
